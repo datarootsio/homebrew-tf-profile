80f8e205ae017315a485e856f84d4b7c89cd2101
Araram96 <ardalan.mehraram.96@gmail.com>
2023-05-23 17:02:08 +0200

    now the file is checking the OS and the chip

--- tf-profile.rb
+++ tf-profile.rb
@@ -1,12 +1,42 @@
 class TfProfile < Formula
-  desc "This is a brewtap which downolads the binary file and make it executable for the following repository https://github.com/datarootsio/tf-profile"
+  desc "This is a brewtap which downloads the binary file and makes it executable for the following repository https://github.com/datarootsio/tf-profile"
   homepage "https://github.com/datarootsio/tf-profile"
-  url "https://github.com/datarootsio/tf-profile/releases/download/release%2F0.2.0/tf-profile-v0.2.0-darwin.zip"
-  sha256 "a866c27a2b2200d38feb542be8a406f40dd1934063f3d84c6e683999bbac01bb"
   license "MIT"
+
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "https://github.com/datarootsio/tf-profile/releases/download/v0.2.1/tf-profile-v0.2.1-darwin-amd64.zip"
+      sha256 "837321de7a93811e9758a1e206389aba1f702de75787d2c9b7f9f389f23010cb"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/datarootsio/tf-profile/releases/download/v0.2.1/tf-profile-v0.2.1-darwin-arm64.zip"
+      sha256 "7655887e8093b5953e4e36e1ac68bab7c08e1cf20dcd25e44ce41ad4f8d1c383"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/datarootsio/tf-profile/releases/download/v0.2.1/tf-profile-v0.2.1-linux-amd64.zip"
+      sha256 "5be66284df9cf363b961d8b15ea28e69048129b9bc96fa13e585c65ec90e16cb"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/datarootsio/tf-profile/releases/download/v0.2.1/tf-profile-v0.2.1-linux-arm64.zip"
+      sha256 "54199c4b2b9c2ad6182884e9a9b5deab320fc65b626a822bc2be04a4f2cb8a76"
+    end
+  elsif OS.windows?
+    if Hardware::CPU.intel?
+      url "https://github.com/datarootsio/tf-profile/releases/download/v0.2.1/tf-profile-v0.2.1-windows-amd64.zip"
+      sha256 "8445b6452375ce0f1cf64645ccd50181b0762e8a9ff984655472d1a26a6a396b"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/datarootsio/tf-profile/releases/download/v0.2.1/tf-profile-v0.2.1-windows-arm64.zip"
+      sha256 "340b614a5e0d194f2410e1df2c7b8c97bb35f596e4ef838da5c7cc8846ce6e8b"
+    end
+  end
 
   def install
     bin.install "tf-profile"
+  end
+
+  def caveats
+    <<~EOS
+      tf-profile has been installed. You can run it using the "tf-profile" command. For additional info, please refer to the following readme: https://github.com/datarootsio/tf-profile/blob/main/README.md
+    EOS
   end
 
   test do
@@ -17,6 +47,5 @@ class TfProfile < Formula
     # Run a simple test command
     output = shell_output("#{bin}/tf-profile --help")
     assert_match "Usage:", output.lines.first.chomp
-    system "false"
   end
 end
